eda0809984cac3b310972f0a8f1f74c0060868df
Havva Yılmaz <86909580+thehavva@users.noreply.github.com>
2021-08-12 00:58:39 +0300

    Add files via upload

---
+++ RGB_LED_2.ino
@@ -0,0 +1,18 @@
+
+void setup() {
+  pinMode(7,OUTPUT);
+  pinMode(6,OUTPUT);
+  pinMode(5,OUTPUT);
+}
+
+void loop() {
+ digitalWrite(7,HIGH);
+ delay(1000);
+ digitalWrite(7,LOW);
+ digitalWrite(6,HIGH);
+ delay(1000);
+ digitalWrite(6,LOW);
+ digitalWrite(5,HIGH);
+ delay(1000);
+ digitalWrite(5,LOW);
+}
